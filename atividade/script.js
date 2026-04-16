5befc1ec0d4fbf284a350b98bb059092367918c0
RobsonA222 <robeson.8991@gmail.com>
2026-04-16 00:23:44 +0000

    Add files via upload

---
+++ atividade/script.js
@@ -0,0 +1,77 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+
+        const li = document.createElement("li");
+
+        li.textContent = task.text;
+
+        if (task.completed) {
+            li.classList.add("completed");
+        }
+
+        li.onclick = () => toggleTask(index);
+
+        const deleteBtn = document.createElement("button");
+
+        deleteBtn.textContent = "X";
+
+        deleteBtn.classList.add("delete-btn");
+
+        deleteBtn.onclick = (event) => {
+            event.stopPropagation();
+            deleteTask(index);
+        };
+
+        li.appendChild(deleteBtn);
+
+        taskList.appendChild(li);
+
+    });
+}
+
+function addTask() {
+
+    if (taskInput.value === "") return;
+
+    tasks.push({
+        text: taskInput.value,
+        completed: false
+    });
+
+    taskInput.value = "";
+
+    saveTasks();
+
+    renderTasks();
+}
+
+function toggleTask(index) {
+
+    tasks[index].completed = !tasks[index].completed;
+
+    saveTasks();
+
+    renderTasks();
+}
+
+function deleteTask(index) {
+
+    tasks.splice(index, 1);
+
+    saveTasks();
+
+    renderTasks();
+}
+
+renderTasks();
